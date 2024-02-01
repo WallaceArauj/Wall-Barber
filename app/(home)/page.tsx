4c4528c77f485116f40da35497bc35547cdb86eb
WallaceArauj <wallace.hostile94@gmail.com>
2024-02-01 18:48:53 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,33 +1,79 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
+
+interface Barbershop {
+  id: string;
+  name: String;
+  address: String;
+  imageUrl: String;
+
+}
 
 export default function Home() {
+  const [barbershops, setBarbershops] = useState<Barbershop[]>([]);
+
+  useEffect(() => {
+    const fetchBarbershops = async () => {
+      try {
+        const fetchedBarbershops = await db.barbershop.findMany({});
+        setBarbershops(fetchedBarbershops);
+      } catch (error) {
+        console.error("Error fetching barbershops:", error);
+      }
+    };
+
+    fetchBarbershops();
+  }, []);
+
   return (
     <div>
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, bem-vindo ao Wall Barber!</h2>
-        <p className="capitalaze text-sm">
-          {format(new Date(), "EEEE',' dd 'de' MMM", {
-          locale: ptBR,
-        })}</p>
+        <h2 className="text-xl font-bold">Olá, Seja bem-vindo!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
       </div>
-        
+
       <div className="px-5 mt-6">
-      <Search />
+        <Search />
       </div>
 
-        <div className="px-5 mt-6">
-          <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-          <BookingItem />
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
+      </div>
 
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
     </div>
-
-
   );
 }
